9c598dbe4ba4cc5f7c1ad620fceb1d7923cb98b9
Humendra Pun <xantoshpun@gmail.com>
2019-10-20 16:32:10 +0545

    Edited

--- app/src/main/java/com/example/radiobutton/MainActivity.java
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    RadioButton rb1, rb2, rb3;
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        rb1 = findViewById(R.id.rbApple);
+        rb2 = findViewById(R.id.rbBanana);
+        rb3 = findViewById(R.id.rbOrange);
+        imgView = findViewById(R.id.imgShow);
+
+        rb1.setOnClickListener(this);
+        rb2.setOnClickListener(this);
+        rb3.setOnClickListener(this);
+
+
+    }
+        @Override
+            public void onClick(View view){
+                switch (view.getId()){
+                    case R.id.rbApple:
+                        imgView.setImageResource(R.drawable.apple);
+                        break;
+
+                    case R.id.rbBanana:
+                        imgView.setImageResource(R.drawable.banana);
+                        break;
+
+                    case R.id.rbOrange:
+                        imgView.setImageResource(R.drawable.orange);
+                        break;
+                }
+
     }
 }
